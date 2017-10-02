24d576e43b2d556eb41cb2c7a80301e0d0e80eea
andreaortega <andreaortegadumont@gmail.com>
2017-10-02 15:52:56 +0200

    2) Col·locar tot el Layout

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -2,6 +2,9 @@ package edu.upc.eseiaat.pma.fakephone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class FakePhoneActivity extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class FakePhoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_phone);
     }
+    public void clicat(View v)
+    {
+        Button boto = (Button) v;
+        Log.i("click","has clicat el boto!");
+    }
+
 }
