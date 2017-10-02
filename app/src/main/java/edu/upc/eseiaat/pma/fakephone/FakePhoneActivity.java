f733fe59f91ce2f3f62d362d75ce0b1e9d6cb597
andreaortega <andreaortegadumont@gmail.com>
2017-10-02 16:56:17 +0200

    4) Funciona everything

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FakePhoneActivity extends AppCompatActivity {
 
     TextView phonenumber;
+    private String aux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +18,30 @@ public class FakePhoneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fake_phone);
 
         phonenumber = (TextView) findViewById(R.id.number);
-
+        aux= ""; /*INICIALITZEM LA VARIABLE*/
        /* String a = "Bond";
         String b = "James";
         String jb = b + a; */
     }
 
+
     public void click(View v)
     {
         Button boto = (Button) v;
         String text_boto = boto.getText().toString();
-        phonenumber.setText(text_boto);
+        aux = aux + text_boto;
+        /*phonenumber.getText(text_boto);*/
+       phonenumber.setText(aux);
+    }
+    public void borrar (View v){
+        Button borrado = (Button) v;
+        aux= "";
+        phonenumber.setText(aux);
+    }
+    public void llamar (View v){
+        Button llamando = (Button) v;
+        aux= "llamando al "+aux;
+        Toast.makeText(FakePhoneActivity.this, aux, Toast.LENGTH_SHORT).show();
     }
 
 }
