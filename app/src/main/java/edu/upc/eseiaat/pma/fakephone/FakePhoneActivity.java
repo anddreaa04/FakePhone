5e5509384b337118e9fb8982e17a5e7c4dccce80
andreaortega <andreaortegadumont@gmail.com>
2017-10-02 16:33:48 +0200

    3) S'ha aconseguit que quan cliques un botó, el numero que conte s'escrigui al TextView

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -2,21 +2,31 @@ package edu.upc.eseiaat.pma.fakephone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FakePhoneActivity extends AppCompatActivity {
+
+    TextView phonenumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_phone);
+
+        phonenumber = (TextView) findViewById(R.id.number);
+
+       /* String a = "Bond";
+        String b = "James";
+        String jb = b + a; */
     }
-    public void clicat(View v)
+
+    public void click(View v)
     {
         Button boto = (Button) v;
-        Log.i("click","has clicat el boto!");
+        String text_boto = boto.getText().toString();
+        phonenumber.setText(text_boto);
     }
 
 }
